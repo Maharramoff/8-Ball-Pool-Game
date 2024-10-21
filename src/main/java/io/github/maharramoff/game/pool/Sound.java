31a54bdc17d854f07daa03289b763836db36041b
Maharramoff <sh.maharramov@gmail.com>
2024-10-21 07:12:37 +0400

    feat: improve Sound class by adding various volume levels

--- src/main/java/io/github/maharramoff/game/pool/Sound.java
+++ src/main/java/io/github/maharramoff/game/pool/Sound.java
@@ -3,9 +3,19 @@ package io.github.maharramoff.game.pool;
 import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public final class Sound
 {
+    public static final float HIGH_VOLUME = 1.0f;
+    public static final float MEDIUM_VOLUME = -3.0f;
+    public static final float LOW_VOLUME = -6.0f;
+    public static final float VERY_LOW_VOLUME = -10.0f;
+    public static final float DEFAULT_VOLUME = -20.0f;
+
+    Logger log = Logger.getLogger("Sound");
+
     public void play(String s)
     {
         getPlay(s, 0.0f);
@@ -19,24 +29,28 @@ public final class Sound
     private void getPlay(String s, float f)
     {
         String              soundDir = "/sounds/";
-        BufferedInputStream path     = new BufferedInputStream(Sound.class.getResourceAsStream(soundDir + "" + s));
+        BufferedInputStream path     = new BufferedInputStream(Objects.requireNonNull(Sound.class.getResourceAsStream(soundDir + s)));
         try
         {
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(path);
             Clip             clip        = AudioSystem.getClip();
             clip.open(audioStream);
             if (clip.isRunning())
+            {
                 clip.stop();
+            }
             clip.setFramePosition(0);
             FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 
             if (f != 0.0f)
+            {
                 gainControl.setValue(f); // Reduce volume by given decibels.
+            }
             clip.start();
         }
         catch (UnsupportedAudioFileException | LineUnavailableException | IOException e)
         {
-            e.printStackTrace();
+            log.severe(e.getMessage());
         }
     }
 }
